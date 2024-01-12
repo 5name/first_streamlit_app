3c119680c7c5768227c71008b926d117a77e8465
Stanislav Lando <stanislav.lando@gmail.com>
2024-01-12 13:08:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+
 
 streamlit.title('Moin zusammen')
 streamlit.header('Header')
@@ -10,3 +10,5 @@ streamlit.header('Header')
 streamlit.text('sonstiges')
 streamlit.text('sonstiges  🥝🍇')
 streamlit.text('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+streamlit.dataframe(my_fruit_list)
